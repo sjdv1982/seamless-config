a2ae88895b5765c03cd2dcf7223ae12a98fb497b
Sjoerd de Vries <sjdv1982@gmail.com>
2026-01-16 17:07:34 +0100

    minor

--- tests/test_remote_clients_collect.py
+++ tests/test_remote_clients_collect.py
@@ -13,13 +13,6 @@ def test_collect_remote_clients_writes_json(tmp_path, monkeypatch):
             {"readonly": False, "url": "http://hash"},
         ],
     }
-    data2 = deepcopy(data)
-    for k in ("database",):
-        for v in data2[k]:
-            for kk in list(v.keys()):
-                if kk == "url":
-                    v["remote_url"] = v["url"]
-                    del v["url"]
 
     class Dummy:
         pass
@@ -27,12 +20,10 @@ def test_collect_remote_clients_writes_json(tmp_path, monkeypatch):
     from seamless_remote import database_remote, buffer_remote
 
     monkeypatch.setattr(
-        database_remote, "inspect_extern_clients", lambda: [data2["database"][0]]
+        database_remote, "inspect_extern_clients", lambda: [data["database"][0]]
     )
     monkeypatch.setattr(database_remote, "inspect_launched_clients", lambda: [])
-    monkeypatch.setattr(
-        buffer_remote, "inspect_extern_clients", lambda: data2["buffer"]
-    )
+    monkeypatch.setattr(buffer_remote, "inspect_extern_clients", lambda: data["buffer"])
     monkeypatch.setattr(buffer_remote, "inspect_launched_clients", lambda: [])
 
     result = collect_remote_clients("dummy-cluster")
